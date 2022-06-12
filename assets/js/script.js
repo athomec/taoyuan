8732e81eb5f168b19c544aebc6fe1ec39d67509c
vagabond <athomec@pie.com.tw>
2022-06-12 21:11:54 +0800

    上傳內頁

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,75 @@ $(function () {//JS開頭
 	var WINDOW = $(window).width();//視窗寬度
 	var WINDOWH = $(window).height();//視窗高度
 
-//---------------------登入頁設定----------------------
-$(".js-toggle-menu").find("input").click(function () {
-	$(this).parents(".js-toggle-menu").find(".form-check").removeClass('active');
-	$(this).parent(".form-check").addClass("active");
-})
-
-//---------------------側邊選單設定------------------------
-$('.js-side-menu').find(".js-toggle-button").click(function () {
-	$(this).toggleClass("active");
-	$(this).siblings('.js-toggle-button').removeClass('active');
-	
-});
-$(".js-side-menu-toggler").click(function () {
-	$('.js-side-menu').toggleClass("close");
-	$(".js-side-content").toggleClass("close");
-	$(this).toggleClass("close");
-})
-//---------------------頁籤設定------------------------
-$('.js-toggle-menu').find(".js-toggle-button").click(function () {
-	$(this).toggleClass("active");
-	$(this).siblings('.js-toggle-button').removeClass('active');
-	
-});
-
-	//---------------------文字縮放設定----------------------
-	$(".js-font-size").find("a").click(function () {
-		$(".js-font-size").find("a").removeClass("now");
-		$(this).addClass("now");
-		if ($(this).attr("data-size") == "txt-sm") {
-			$("body").css('font-size', '.9em');
-		} else if ($(this).attr("data-size") == "txt-md") {
-			$("body").css('font-size', '1em');
-		} else if ($(this).attr("data-size") == "txt-lg") {
-			$("body").css('font-size', '1.1em');
-		}
-		return false;
+	//---------------------登入頁設定----------------------
+	$(".js-toggle-menu").find("input").click(function () {
+		$(this).parents(".js-toggle-menu").find(".form-check").removeClass('active');
+		$(this).parent(".form-check").addClass("active");
 	})
 
-	
+	//---------------------側邊選單設定------------------------
+	$(".js-info-opener").click(function () {
+		let title = $(this).find("span").text();
+		let icon = $(this).find("img").attr("src");
+		let content = $(this).attr("data-target");
+		//開啟資料標題
+		$(".js-handler").find("span").text(title);
+		$(".js-handler").find("img").attr("src", icon);
+		$(".js-handler").toggleClass('open');
+		//地圖收起
+		$(".js-map-content").toggleClass('open');
+		
+		//目標是否有資料
+		$(content).toggleClass('open');
+		console.log(content)
+	})
+	$(".js-side-menu-toggler").click(function () {
+		$('.js-side-menu').toggleClass("close");
+		$(".js-side-content").toggleClass("close");
+	})
+	//---------------------視窗拖曳設定------------------------
+	var handler = document.querySelector('.js-handler');
+	var wrapper = handler.closest('.js-side-content');
+	var boxA = wrapper.querySelector('.js-map-content');
+	var boxB = wrapper.querySelector('.js-side-info');
+	var isHandlerDragging = false;
+	var isHandlertoching = false;
+
+	document.addEventListener('mousedown', function (e) {
+		// If mousedown event is fired from .handler, toggle flag to true
+		if (e.target === handler) {
+			isHandlerDragging = true;
+		}
+	});
+
+	document.addEventListener('mousemove', function (e) {
+		// Don't do anything if dragging flag is false
+		if (!isHandlerDragging) {
+			return false;
+		}
+		// Get offset
+		var containerOffsetTop = wrapper.offsetTop;
+		var containerOffsetBottom = wrapper.offsetBottom;
+		// Get x-coordinate of pointer relative to container
+		var pointerRelativeXpos = e.clientY - containerOffsetTop;
+		var pointerRelativeXpos2 = e.clientY - e.offsetTop + e.offsetHeight;
+		var boxAminWidth = 30;
+		boxA.style.height = (Math.max(boxAminWidth, pointerRelativeXpos - 2)) + 'px';
+		//boxA.style.flexGrow = 0;
+		boxB.style.height = (Math.max(boxAminWidth, pointerRelativeXpos2 - 8)) + 'px';
+		//boxB.style.flexGrow = 0;
+	});
+	document.addEventListener('mouseup', function (e) {
+		// Turn off dragging flag when user mouse is up
+		isHandlerDragging = false;
+	});
+
+	//---------------------頁籤設定------------------------
+	$('.js-toggle-menu').find(".js-toggle-button").click(function () {
+		$(this).toggleClass("active");
+		$(this).siblings('.js-toggle-button').removeClass('active');
+
+	});
 
 	//手風琴按鈕切換設定
 	$(".js-accordion-menu").find('button').click(function () {
@@ -55,33 +85,6 @@ $('.js-toggle-menu').find(".js-toggle-button").click(function () {
 			$(this).find('span').removeClass('ti-angle-down').addClass('ti-angle-up');
 		}
 
-	});
-
-	//地圖切換按鈕設定
-	$(".js-side-toggle").find(".js-toggle-menu").find(".btn").click(function () {
-		$(".js-side-menu").addClass("open");
-		$(".js-side-toggle").addClass("open");
-		$(".js-side-content").addClass("open");
-	})
-	//地圖收合按鈕設定
-	$(".js-map-toggler").click(function () {
-		if($("#layer-all").hasClass("show")){
-			$("#layer-all").removeClass("show");
-			$("#locate-all").addClass("show");
-		}else{
-			$("#locate-all").addClass("show");
-		}
-
-		$(".js-side-menu").toggleClass("open");
-		$(".js-side-toggle").toggleClass("open");
-		$(".js-side-content").toggleClass("open");
-		$(this).toggleClass("open");
-		if($(this).hasClass("open")){
-			$('[data-bs-target="#locate-all"]').addClass("active");
-		}else{
-			$(".js-side-toggle").find(".js-toggle-menu").find(".btn").removeClass("active")
-		}
-		
 	});
 
 	//----------------gotop功能-----------------
@@ -101,18 +104,6 @@ $('.js-toggle-menu').find(".js-toggle-button").click(function () {
 		var index = 0;//各單元區塊順序
 		var st = $(window).scrollTop();//現在捲軸位置
 		var wh = $(window).height();//視窗高度
-
-		/*$(".section").each(function (i, element) {//選單標示設定
-			if (st > $(".section").eq(i).offset().top + $(".section").eq(i).outerHeight(true) / 2 - wh) {//i離開畫面時
-				index = i;
-				$('nav li').find("a").css('color', '#FFFFFF');
-				$('nav li').eq(index - 1).find("a").css('color', '#FCE76D');
-			}
-			
-			if (st < $(".section").eq(1).offset().top - 84 ) {
-				$('nav li').find("a").css('color', '#FFFFFF');
-			}
-		});*/
 	});
 
 	//主選單設定
@@ -122,76 +113,21 @@ $('.js-toggle-menu').find(".js-toggle-button").click(function () {
 		$(".navbar-collapse").removeClass("show");
 		return false;
 	});
-	
-	//消息輪播設定
-	$('.flood-slider').slick({
-		dots: true,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive: [
-		  {
-			breakpoint: 1200,
-			settings: {
-			  slidesToShow: 2,
-			  slidesToScroll: 2,
-			  infinite: true,
-			  dots: true
-			}
-		  },
-		  {
-			breakpoint: 768,
-			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1
-			}
-		  }
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
-		]
-	  });
 
-	  $('.disaster-slider').slick({
-		dots: true,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive: [
-		  {
-			breakpoint: 1200,
-			settings: {
-			  slidesToShow: 2,
-			  slidesToScroll: 2,
-			  infinite: true,
-			  dots: true
-			}
-		  },
-		  {
-			breakpoint: 768,
-			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1
-			}
-		  }
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
-		]
-	  });
 
 	RESIZE();
+
+	/*$(window).resize(function () {
+		RESIZE();
+	});*/
 
 	function RESIZE() {
 		WINDOWH = $(window).height();
 		WINDOW = $(window).width();
 
-		if (WINDOW < 1200) {
-			$("#locate-all").removeClass("show");
-		} else {
-			$("#locate-all").addClass("show");
+		if (WINDOW < 768) {
+			$('.js-side-menu').addClass("close");
+			$(".js-side-content").addClass("close");
 		}
 	}
 
